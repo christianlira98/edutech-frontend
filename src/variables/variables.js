eedd8133ca298dd4f539ef250a9fb5cc4ccfb6ea
christianlira98 <christianlira98@gmail.com>
2021-07-04 15:24:45 -0300

    changes

--- src/variables/variables.js
+++ src/variables/variables.js
@@ -1,6 +1,6 @@
 import ax from 'axios'
 
 // Axios to use on project
-const axios = ax.create({ baseURL: process.env.VUE_APP_API_BASE_URL +'/api'})
+const axios = ax.create({ baseURL: process.env.VUE_APP_API_BASE_URL + '/api' })
 
 export default axios
